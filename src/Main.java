8f1af45167dfbc95194338d02cafc1ed05a1489e
Nebot774 <adriclemenbot@gmail.com>
2023-10-30 23:43:28 +0100

    reajustes para examen

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,42 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        /**
+         *Haz un programa que reciba como argumento la ruta de un directorio y “aplane” (flatten) su contenido, es
+         * decir, que mueva a este mismo directorio todos los ficheros que tenga anidados. También tendrá que borrar
+         * todos los directorios anidados. Por ejemplo, si ejecutamos el programa con este directorio "niats":
+         *
+         *
+         *
+         * niats
+         * │
+         * ├─ aaa
+         * │  ├─ a.txt
+         * │  ├─ bbb
+         * │  │  ├─ b.txt
+         * │  ├─ ccc
+         * │  │  ├─ c.txt
+         * │  │  ├─ ddd
+         * │  │  │  ├─ d.txt
+         * │  ├─ eee
+         * │  │  ├─ e.txt
+         * │
+         * ├─ fff
+         * │  ├─ f.txt
+         * │
+         * └─ n.txt
+         *
+         *
+         *
+         *
+         *
+         */
+
+
+        // Definición de la ruta principal del directorio 'niats' en '/tmp'
         Path sourceDirectory = Paths.get("/tmp/niats"); // Reemplaza con la ruta de tu directorio
 
-
+        // Definición de subdirectorios dentro de 'niats' o de otros subdirectorios
         Path directoryAAA = Paths.get(sourceDirectory.toString(), "aaa");
         Path directoryBBB = Paths.get(directoryAAA.toString(), "bbb");
         Path directoryCCC = Paths.get(directoryAAA.toString(), "ccc");
@@ -22,6 +55,7 @@ public class Main {
         Path directoryEEE = Paths.get(directoryAAA.toString(), "eee");
         Path directoryFFF = Paths.get(sourceDirectory.toString(), "fff");
 
+        // Definición de rutas para archivos dentro de los directorios especificados
         Path fileA = Paths.get(directoryAAA.toString(), "a.txt");
         Path fileB = Paths.get(directoryBBB.toString(), "b.txt");
         Path fileC = Paths.get(directoryCCC.toString(), "c.txt");
@@ -30,6 +64,7 @@ public class Main {
         Path fileF = Paths.get(directoryFFF.toString(), "f.txt");
         Path fileN = Paths.get(sourceDirectory.toString(), "n.txt");
 
+        // Creación de directorios. Si el directorio ya existe, no hará nada.
         Files.createDirectories(directoryAAA);
         Files.createDirectories(directoryBBB);
         Files.createDirectories(directoryCCC);
@@ -37,6 +72,7 @@ public class Main {
         Files.createDirectories(directoryEEE);
         Files.createDirectories(directoryFFF);
 
+        // Creación de archivos. Si el archivo ya existe, lanzará una excepción.
         Files.createFile(fileA);
         Files.createFile(fileB);
         Files.createFile(fileC);
@@ -78,6 +114,53 @@ public class Main {
                         }
                     });
         }
+
+        /**
+         * APUNTES Y CONCEPTOS CLAVE:
+         *
+         * 1. Java NIO (New I/O) y java.nio.file:
+         * - java.nio.file.Path: Representa una ruta en el sistema de archivos. Es una alternativa moderna y flexible a la clase File tradicional.
+         * - java.nio.file.Paths: Proporciona métodos estáticos para obtener objetos Path.
+         * - java.nio.file.Files: Proporciona métodos para operaciones de archivos y directorios.
+         *
+         * 2. Uso de Paths.get():
+         * - Se utiliza para crear un objeto Path, que puede representar un archivo o un directorio.
+         *
+         * 3. Creación de directorios y archivos:
+         * - Files.createDirectories(path): Crea todos los directorios en la ruta especificada. Si ya existen, no hace nada.
+         * - Files.createFile(path): Crea un nuevo archivo vacío en la ruta especificada.
+         *
+         * 4. Recorrer directorios y subdirectorios:
+         * - Files.walk(path): Devuelve un Stream que es una vista de todos los archivos y directorios en un directorio y subdirectorios.
+         *
+         * 5. Java Streams:
+         * - Se utiliza para procesar colecciones de datos de manera funcional.
+         *
+         * 6. Recopilación en una lista:
+         * - collect(Collectors.toList()): Convierte el Stream en una lista.
+         *
+         * 7. Verificar si un Path es un archivo regular:
+         * - Files.isRegularFile(path): Retorna true si el Path representa un archivo regular (no un directorio).
+         *
+         * 8. Mover archivos:
+         * - Files.move(source, destination, options): Mueve o renombra un archivo a una ruta de destino, con opciones como StandardCopyOption.REPLACE_EXISTING para reemplazar si el archivo ya existe.
+         *
+         * 9. Borrar directorios y archivos de manera recursiva:
+         * - Para borrar directorios anidados de forma segura, es necesario ordenarlos en orden inverso (primero borrar los directorios más profundos) y asegurarse de que el directorio esté vacío antes de borrarlo.
+         *
+         * 10. Manejo de excepciones:
+         * - Las operaciones de archivo pueden lanzar excepciones, en particular IOException, que debe ser capturada o declarada en el método.
+         *
+         * OBJETIVO:
+         * - El programa tiene como objetivo "aplanar" un directorio, es decir, mover todos los archivos de los subdirectorios al directorio principal y luego eliminar todos los subdirectorios.
+         *
+         * PROCESO:
+         * 1. Crea una estructura de directorios y archivos para probar el proceso de aplanamiento.
+         * 2. Utiliza Files.walk() para obtener una lista de todos los archivos y directorios dentro del directorio principal.
+         * 3. Mueve todos los archivos al directorio principal.
+         * 4. Elimina todos los subdirectorios de manera recursiva.
+         */
+
 
 
 
